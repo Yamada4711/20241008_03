445f2c79aae596828e1910b87c78e6c1cf67c928
Saki Yamada <sichimi_0331-school@yahoo.co.jp>
2024-10-09 21:06:37 +0900

    プロジェクト ファイルを追加します。

---
+++ 20241008_03/20241008_03.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+static int mult(int x, int y);
+
+int main()
+{
+	int x = 2, y = 5;
+
+	int ans = mult(x, y);
+
+	cout << x << " * " << y << " = " << ans << endl;
+}
+
+int mult(int x, int y)
+{
+	return x * y;
+}
